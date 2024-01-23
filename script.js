946bcbee074d9ff27264f534d32785f73ef52400
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-01-23 14:38:11 +0000

    Made further attempts to code the colour change

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-
+//Code to print the current date at the top of the page
 let currentDay = document.getElementById("currentDay");
-
 // Get the current date using Day.js
 var currentDate = dayjs();
-
 // Display the current date
 currentDay.innerText = currentDate.format("D MMM YYYY");
 
 
-
-//Function to save data
-function saveData(rowNumber) {
-    var inputValue = document.querySelectorAll('input')[rowNumber].value;
-    console.log("Saving data:", inputValue);
-    // Add your save logic here
-  }
-
-// Function to update the colour of the task based on the time 
+//Function to change colour of row based on time 
 function updateColors() {
     var currentTime = dayjs().format('HH:mm');
     
-    document.querySelectorAll('.time-block').forEach(function(timeBlock) {
-      var blockTime = timeBlock.textContent;
-      var row = timeBlock.closest('tr');
+    document.querySelectorAll('.time-block.hour time').forEach(function(timeElement) {
+      var blockTime = timeElement.textContent.trim();
+      var row = timeElement.closest('tr');
 
-      if (blockTime < currentTime) {
+      var blockTimeObj = dayjs(blockTime, 'HH:mm');
+      var currentTimeObj = dayjs(currentTime, 'HH:mm');
+
+      if (blockTimeObj.isBefore(currentTimeObj)) {
         row.classList.remove('present', 'future');
         row.classList.add('past');
-      } else if (blockTime === currentTime) {
+      } else if (blockTimeObj.isSame(currentTimeObj, 'hour')) {
         row.classList.remove('past', 'future');
         row.classList.add('present');
       } else {
@@ -35,4 +28,6 @@ function updateColors() {
         row.classList.add('future');
       }
     });
-  }
+}
+
+
